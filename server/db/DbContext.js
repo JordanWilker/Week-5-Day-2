77929f4c655a7280e2e4de97285df7f53405f9c8
Jordan Wilkerson <jordangwilkerson@gmail.com>
2021-03-02 15:50:04 -0700

    working?

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,10 +1,14 @@
 import ValueSchema from "../models/Value";
 import CarSchema from "../models/Car";
+import JobSchema from "../models/Job";
+import HouseSchema from "../models/House"
 import mongoose from "mongoose";
 
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
   Cars = mongoose.model("Car", CarSchema);
+  Jobs = mongoose.model("Job", JobSchema)
+  Houses = mongoose.model("House", HouseSchema)
 }
 
 export const dbContext = new DbContext();
---
+++ server/models/House.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const House = new Schema(
+  {
+    bedrooms: { type: String, required: true },
+    bathrooms: { type: String, required: true },
+    description: { type: String },
+    levels: { type: Number, required: true },
+    year: { type: Number, required: true  },
+    price: {type: Number, required: true},
+    imgUrl: { type: String, required: true, default: "http://placehold.it/200x200" }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+export default House;
